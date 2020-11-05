8352b7f78d21bafa70f6001d40564e4fd006add4
USIS044220 <68661277+USIS044220@users.noreply.github.com>
2020-11-04 19:22:04 -0600

    usuarios

---
+++ SistemaDeFarmacia/DATA/db_conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace SistemaDeFarmacia.DATA
+{
+    class db_conexion
+    {
+        SqlConnection miConexion = new SqlConnection();
+        SqlCommand comandosSQL = new SqlCommand();
+        SqlDataAdapter miAdaptadorDatos = new SqlDataAdapter();
+
+        DataSet ds = new DataSet();
+    }
+}
